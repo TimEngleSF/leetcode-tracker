57cf28f539e58909397bd1c9f7d2296d82a30375
Tim Engle <122645876+TimEngleSF@users.noreply.github.com>
2023-09-03 02:37:25 -0700

    Delete api/server/db/schemas/auth.js

--- api/server/db/schemas/auth.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-export const registerSchema = Joi.object({
-    username: Joi.string().required(),
-    firstName: Joi.string().required(),
-    lastInit: Joi.string().required(),
-    yob: Joi.string().required(),
-    password: Joi.string().required(),
-});
