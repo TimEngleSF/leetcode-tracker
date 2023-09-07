50bd7e68feb0da7afd7a6c0be708c47e8ff49aa7
Tim Engle <timothy.englew@gmail.com>
2023-09-07 13:41:01 -0700

    🪺 Fix nested function

--- cli/src/mainLoop.ts
+++ cli/src/mainLoop.ts
@@ -12,68 +12,68 @@ const mainLoop = async () => {
   let questNum: any;
   let viewPrevQuest = false;
 
-  while (isRunning) {
-    loop: while (isRunning) {
-      // Section for Breaking loop
-      if (viewPrevQuest) {
-        await getAllUserQuestsByQuestNum(questNum);
-        const action = await inquirer.prompt({
-          type: 'confirm',
-          name: 'continue',
-          message: 'Do you want to continue?',
-          default: true,
-        });
-        if (action.continue) {
-          viewPrevQuest = false;
-        } else {
-          isRunning = false;
-          break loop; // This will break the labeled loop
-        }
+  loop: while (isRunning) {
+    // Section for Breaking loop
+    if (viewPrevQuest) {
+      await getAllUserQuestsByQuestNum(questNum);
+      const action = await inquirer.prompt({
+        type: 'confirm',
+        name: 'continue',
+        message: 'Do you want to continue?',
+        default: true,
+      });
+      if (action.continue) {
+        console.clear();
+        viewPrevQuest = false;
+      } else {
+        isRunning = false;
+        break loop;
       }
+    }
 
-      // Main Section
-      const action = await inquirer.prompt([
-        {
-          type: 'list',
-          name: 'nextAction',
-          message: chalk.greenBright('What would you like to do?'),
-          choices: [
-            { name: 'Add Question Result', value: 'addQuestion' },
-            'Review Questions',
-            'View History',
-            'Leaderboard',
-            'Login',
-            'Logout',
-            'Exit',
-          ],
-        },
-      ]);
-      console.clear();
-      printHeader();
+    // Main Section
+    const action = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'nextAction',
+        message: chalk.greenBright('What would you like to do?'),
+        choices: [
+          { name: 'Add Question Result', value: 'addQuestion' },
+          'Review Questions',
+          'View History',
+          'Leaderboard',
+          'Login',
+          'Logout',
+          'Exit',
+        ],
+      },
+    ]);
+    console.clear();
+    printHeader();
 
-      switch (action.nextAction) {
-        case 'Login':
-          console.log(chalk.green('Logging in...'));
-          await loginUser();
-          break;
-        case 'addQuestion':
-          console.log(chalk.green('Adding question...'));
-          questNum = await addQuestionToDB();
-          viewPrevQuest = await viewPrevQuestPrompt();
+    switch (action.nextAction) {
+      case 'Login':
+        console.log(chalk.green('Logging in...'));
+        await loginUser();
+        break;
+      case 'addQuestion':
+        console.log(chalk.green('Adding question...'));
+        questNum = await addQuestionToDB();
+        viewPrevQuest = await viewPrevQuestPrompt();
 
-          console.clear();
-          printHeader();
-          break;
-        case 'Dummy Option B':
-          console.log(chalk.green('Executing dummy option B'));
-          // Code
-          break;
-        case 'Exit':
-          console.log(chalk.red('Exiting LeetCode Tracker'));
-          isRunning = false;
-          break;
-      }
+        console.clear();
+        printHeader();
+        break;
+      case 'Dummy Option B':
+        console.log(chalk.green('Executing dummy option B'));
+        // Code
+        break;
+      case 'Exit':
+        console.log(chalk.red('Exiting LeetCode Tracker'));
+        isRunning = false;
+        break;
     }
   }
 };
+
 export default mainLoop;
