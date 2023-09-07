066067eab1b64927ed8bfd513d0ad4f5b4eac57e
Tim Engle <timothy.englew@gmail.com>
2023-09-07 13:34:59 -0700

    ⬇️ Added functionality to get prev attempts after adding question

---
+++ cli/src/Prompts/viewPrevQuestPrompt.ts
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import getAllUserQuestsByQuestNum from '../Questions/getAllUserQuestsByQuestNum.js';
+
+const viewPrevQuestPrompt = async () => {
+  const answers: { viewPrev: boolean } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'viewPrev',
+      message:
+        'Would you like to view your previous attempts for this question?',
+      choices: [
+        { name: chalk.green('Yes'), value: true },
+        { name: chalk.red('No'), value: false },
+      ],
+    },
+  ]);
+
+  return answers.viewPrev;
+};
+
+export default viewPrevQuestPrompt;
