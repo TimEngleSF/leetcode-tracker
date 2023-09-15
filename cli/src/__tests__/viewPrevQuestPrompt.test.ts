431fbb62b3730cfe3d55bd7e5119342a497004bd
Tim Engle <timothy.englew@gmail.com>
2023-09-15 14:22:54 -0700

    🧪 Refactor addQuestionPrompt tests, added tests for viewPrevQuestPrompt

---
+++ cli/src/__tests__/viewPrevQuestPrompt.test.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import viewPrevQuestPrompt from '../Questions/Prompts/viewPrevQuestPrompt.js';
+
+describe('viewPrevQuestionPrompt', () => {
+  const selectionPrompt: any = sinon.stub();
+  beforeEach(() => {
+    selectionPrompt.reset();
+  });
+
+  it('should return a boolean', async () => {
+    selectionPrompt.resolves({ viewPrev: true });
+    const result = await viewPrevQuestPrompt(selectionPrompt);
+    expect(typeof result).to.eq('boolean');
+  });
+
+  it('should return true when the user selects true', async () => {
+    selectionPrompt.resolves({ viewPrev: true });
+    const result = await viewPrevQuestPrompt(selectionPrompt);
+    expect(result).to.be.true;
+  });
+
+  it('should return false when the user selects false', async () => {
+    selectionPrompt.resolves({ viewPrev: false });
+    const result = await viewPrevQuestPrompt(selectionPrompt);
+    expect(result).to.be.false;
+  });
+});
