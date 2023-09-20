6eb3b2a602d4f0163b649e423a7293dfe98623dd
Tim Engle <timothy.englew@gmail.com>
2023-09-20 16:57:10 -0700

    📚 Added functionality to get Review Questions and add review results

---
+++ cli/src/ReviewQuestions/helpers/getReviewQuestions.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import writeErrorToFile from '../../errors/writeError.js';
+import { getAuthHeaders } from '../../utils.js';
+
+export const getReviewQuestions = async (
+  reviewRangeSelection: { olderThan: number; newerThan: number },
+  axiosInstance = axios,
+  authHeadersInstance = getAuthHeaders,
+  writeErrorInstance = writeErrorToFile
+) => {
+  try {
+    const authHeaders = await authHeadersInstance();
+    const { data, status } = await axiosInstance({
+      method: 'GET',
+      url: 'http://localhost:3000/review',
+      headers: authHeaders,
+      data: reviewRangeSelection,
+    });
+    if (status === 200) {
+      return data;
+    } else {
+      return false;
+    }
+  } catch (error: any) {
+    await writeErrorInstance(
+      error,
+      'Error arrised while executing generalLeaderBoard function'
+    );
+  }
+};
+
+console.log(await getReviewQuestions({ olderThan: 7, newerThan: 14 }));
