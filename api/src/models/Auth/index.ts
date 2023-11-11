637ba969fe2cb79910fe1f77152eec68b499e686
Tim Engle <timothy.englew@gmail.com>
2023-11-11 14:57:09 -0800

    🗑️ Removed outdated model/Auth folder

--- api/src/models/Auth/index.ts
+++
@@ -1,9 +0,0 @@
-import { validateSecAnswer } from './validateSecAnswer.js';
-import { resetPass } from './resetPass.js';
-
-const AuthModel = {
-  validateSecAnswer,
-  resetPass,
-};
-
-export default AuthModel;
