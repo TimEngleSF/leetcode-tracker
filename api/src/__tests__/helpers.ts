6b0726d02bbf468273670c7df00ac4a3abf4b860
Tim Engle <timothy.englew@gmail.com>
2023-11-19 02:08:27 -0800

    🧪❔Added tests for Question model current coverage 77%

--- api/src/__tests__/helpers.ts
+++ api/src/__tests__/helpers.ts
@@ -1,4 +1,6 @@
-import { Db, MongoClient } from 'mongodb';
+import fs from 'fs/promises';
+import path from 'path';
+import { Db, MongoClient, ObjectId } from 'mongodb';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import User from '../models/User';
 import Question from '../models/Question';
@@ -19,9 +21,26 @@ export const createMockDb = async (): Promise<MockDb> => {
     const db = client.db('lc-test-db');
     User.injectDb(db);
     Question.injectDb(db);
+
+    const questionDataJsonPath = path.join(__dirname, 'questionInfo.json');
+
+    const questionDataJson = JSON.parse(
+      await fs.readFile(questionDataJsonPath, 'utf-8')
+    );
+    const transformedData = questionDataJson.map((item: any) => {
+      if (item._id && item._id.$oid) {
+        return { ...item, _id: new ObjectId(item._id.$oid) };
+      }
+      return item;
+    });
+
+    if ((await db.collection('questionData').countDocuments()) === 0) {
+      await db.collection('questionData').insertMany(transformedData);
+    }
+
     const MockDb = { db, client, uri, mongoServer };
     return MockDb;
-  } catch (error) {
-    throw new Error('Cannot to mock db: ${error.message}');
+  } catch (error: any) {
+    throw new Error(`Cannot to mock db: ${error.message}`);
   }
 };
--- api/src/__tests__/models/Questions.test.ts
+++ api/src/__tests__/models/Questions.test.ts
@@ -4,21 +4,24 @@ import {
   Collection,
   ObjectId,
   InsertOneResult,
+  FindCursor,
 } from 'mongodb';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { faker } from '@faker-js/faker';
-import bcrypt from 'bcrypt';
 import chai from 'chai';
 import sinon, { SinonStub } from 'sinon';
-// import User from '../../models/User';
+
 import { closeDb } from '../../db/connection';
 import { UserDocument } from '../../types/userTypes';
 import { ExtendedError } from '../../errors/helpers';
 import Question from '../../models/Question';
 import { createMockDb } from '../helpers';
 import User from '../../models/User';
-import { QuestionDocument } from '../../types/questionTypes';
-import exp from 'constants';
+import {
+  QuestionDocument,
+  QuestionInfoDocument,
+} from '../../types/questionTypes';
+import { convertDaysToMillis } from '../../models/helpers/questionHelpers';
 
 const { expect } = chai;
 
@@ -27,6 +30,10 @@ const stub = {
     findOneStub: (): SinonStub =>
       sinon
         .stub(Collection.prototype, 'findOne')
+        .throws(new Error('Simulated Error')),
+    findStub: (): SinonStub =>
+      sinon
+        .stub(Collection.prototype, 'find')
         .throws(new Error('Simulated Error')),
     findOneAndUpdate: () =>
       sinon
@@ -53,17 +60,65 @@ const stub = {
       sinon
         .stub(Collection.prototype, 'insertOne')
         .resolves({ insertedId: new ObjectId(), acknowledged: false }),
+    findStub: () =>
+      sinon.stub(Collection.prototype, 'find').returns({
+        toArray: () => Promise.resolve([]),
+      } as unknown as FindCursor<Document>),
   },
 };
 
-const testErrorTransformation = async (action: Function) => {
+const testErrorTransformation = async (
+  action: Function,
+  statusCode: number
+) => {
   try {
     await action();
     expect.fail('Expected an error but none was thrown');
   } catch (error: any) {
     expect(error).to.be.an.instanceOf(ExtendedError);
-    expect(error.statusCode).to.be.equal(500);
+    expect(error.statusCode).to.be.equal(statusCode);
+    expect(error.statck).to.not.be.null;
   }
+};
+
+const addQuestionToDb = async (newQuestion: {
+  userId: string | ObjectId;
+  username: string;
+  questNum: number;
+  created: Date;
+  speed: number | undefined;
+  passed: boolean;
+}) => {
+  await Question.addQuestion({ ...newQuestion });
+};
+
+const generateNewUserQuestionPayload = (
+  userId: ObjectId | 'string',
+  username: string,
+  questNum: number,
+  date: { fromDaysAgo: number; toDaysAgo: number }
+) => {
+  if (date.fromDaysAgo < date.toDaysAgo) {
+    throw new Error('date.fromDays ago must be greater than date.toDaysAgo');
+  }
+  let payloadPassed = faker.datatype.boolean();
+  const generateDate = (daysAgo: number) =>
+    new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      new Date().getDate() - daysAgo
+    );
+  return {
+    userId,
+    username,
+    questNum,
+    passed: payloadPassed,
+    speed: payloadPassed ? faker.number.int({ min: 50, max: 4000 }) : undefined,
+    created: faker.date.between({
+      from: generateDate(date.fromDaysAgo),
+      to: generateDate(date.toDaysAgo),
+    }),
+  };
 };
 
 describe('Question model', () => {
@@ -73,6 +128,7 @@ describe('Question model', () => {
   let uri: string;
   let findOneStub: SinonStub;
   let insertOneStub: SinonStub;
+  let findStub: SinonStub;
 
   // Insert data
   let newUser: UserDocument;
@@ -108,27 +164,12 @@ describe('Question model', () => {
     if (insertOneStub) {
       insertOneStub.restore();
     }
+    if (findStub) {
+      findStub.restore();
+    }
   });
 
   describe('add question', () => {
-    beforeEach(async () => {
-      if (findOneStub) {
-        findOneStub.restore();
-      }
-      if (insertOneStub) {
-        insertOneStub.restore();
-      }
-    });
-
-    afterEach(() => {
-      if (findOneStub) {
-        findOneStub.restore();
-      }
-      if (insertOneStub) {
-        insertOneStub.restore();
-      }
-    });
-
     it("should add a user's question data", async () => {
       questNum = faker.number.int({ min: 1, max: 2400 });
       randBoolean = faker.datatype.boolean();
@@ -160,23 +201,28 @@ describe('Question model', () => {
 
     it('should throw a transformed error if insertion is not acknowledged', async () => {
       insertOneStub = stub.null.insertOneStub();
-      await testErrorTransformation(() =>
-        Question.addQuestion({
-          userId: newUser._id,
-          username: newUser.username,
-          questNum,
-          passed: randBoolean,
-          speed,
-          created: date,
-        })
+      await testErrorTransformation(
+        () =>
+          Question.addQuestion({
+            userId: newUser._id,
+            username: newUser.username,
+            questNum,
+            passed: randBoolean,
+            speed,
+            created: date,
+          }),
+        500
       );
       insertOneStub.restore();
     });
   });
 
   describe('get questions methods', () => {
+    const urlRegex =
+      /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
+
     describe('getQuestion', () => {
-      it('should getQuestion by questId string', async () => {
+      it('should getQuestion by questId string and return the correct data', async () => {
         const result = await Question.getQuestion(
           insertedResult._id.toHexString()
         );
@@ -210,6 +256,107 @@ describe('Question model', () => {
           expect(error.statusCode).to.equal(500);
           expect(error.stack).to.not.be.null;
         }
+      });
+    });
+    describe('getQuestionInfo', () => {
+      it('should get question info by a number and return the correct data', async () => {
+        const randNum = faker.number.int({ min: 1, max: 2000 });
+        const result = await Question.getQuestionInfo(randNum);
+        expect(result._id).to.be.an.instanceOf(ObjectId);
+        expect(['Easy', 'Medium', 'Hard']).to.include(result.diff);
+        expect(result.title).to.be.a('string');
+        expect(result.url).to.match(urlRegex);
+      });
+      it('should throw a transformed error with statusCode 404 if query returns null', async () => {
+        findOneStub = stub.null.findOneStub();
+        await testErrorTransformation(
+          () => Question.getQuestionInfo(5000),
+          404
+        );
+        findOneStub.restore();
+      });
+      it('should throw a transformed error with statusCode 500 if query method encounters an error', async () => {
+        const randNum = faker.number.int({ min: 1, max: 2000 });
+        findOneStub = stub.error.findOneStub();
+        await testErrorTransformation(
+          () => Question.getQuestionInfo(randNum),
+          500
+        );
+      });
+    });
+    describe('getQuestionsByUser', () => {
+      before(async () => {
+        let questionPayload = {};
+        for (let i = 0; i < 4; i++) {
+          await addQuestionToDb(
+            generateNewUserQuestionPayload(newUser._id, newUser.username, 22, {
+              fromDaysAgo: 10,
+              toDaysAgo: 5,
+            })
+          );
+        }
+      });
+      it('should get an array of questions by userId', async () => {
+        const result = await Question.getQuestionsByUser(newUser._id);
+        expect(result).to.be.an('array');
+        expect(result.length).to.be.greaterThan(1);
+        result.forEach((document) => {
+          expect(document.questNum).to.be.a('number');
+          expect(document.created).to.be.a('date');
+          expect(document.passed).to.be.a('boolean');
+          if (document.speed !== null) {
+            expect(document.speed).to.be.a('number');
+          } else {
+            expect(document.speed).to.be.null;
+          }
+        });
+      });
+
+      it('should get an array of questions by userId and questId number', async () => {
+        const result = await Question.getQuestionsByUser(newUser._id, 22);
+        expect(result).to.be.an('array');
+        expect(result.length).to.be.greaterThan(1);
+        result.forEach((document) => {
+          expect(document.created).to.be.a('date');
+          expect(document.passed).to.be.a('boolean');
+          if (document.speed !== null) {
+            expect(document.speed).to.be.a('number');
+          } else {
+            expect(document.speed).to.be.null;
+          }
+        });
+      });
+
+      it('should return an empty array if there are no results', async () => {
+        findStub = stub.null.findStub();
+        const result = await Question.getQuestionsByUser(newUser._id, 22);
+        expect(result).to.be.an('array');
+        expect(result).to.have.length(0);
+      });
+
+      it('should throw a transformed error if an error is encountered while querying db', async () => {
+        findStub = stub.error.findStub();
+        await testErrorTransformation(
+          () => Question.getQuestionsByUser(newUser._id),
+          500
+        );
+      });
+    });
+
+    describe('getReviewQuestions', () => {
+      let result: QuestionInfoDocument[];
+      before(async () => {
+        result = await Question.getReviewQuestions(newUser._id, 0, 10);
+      });
+      it('should return an array', async () => {
+        expect(result).to.be.an('array');
+      });
+      it('should contain QuestionInfoDocuments', () => {
+        expect(result[0]._id).to.be.an.instanceOf(ObjectId);
+        expect(result[0].title).to.be.a('string');
+        expect(result[0].questId).to.be.a('number');
+        expect(result[0].diff).to.be.a('string');
+        expect(result[0].url).to.match(urlRegex);
       });
     });
   });
