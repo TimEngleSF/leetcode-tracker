710e8804d37eed2d6d7becc6012aa62e4c49683b
Tim Engle <timothy.englew@gmail.com>
2023-11-20 11:57:18 -0800

    🖥️Added feature to display version or notification to update.

--- cli/src/config.ts
+++ cli/src/config.ts
@@ -1,1 +1,15 @@
-export const API_URL = 'http://localhost:3000';
+import { getPackageVersion } from './utils.js';
+
+export let API_URL = 'http://localhost:3000';
+export let LATEST_VERSION = process.env.npm_package_version;
+
+export const setLatestVersion = async () => {
+  const actualLatestVersion = await getPackageVersion();
+  try {
+    if (actualLatestVersion !== LATEST_VERSION) {
+      LATEST_VERSION = actualLatestVersion;
+    }
+  } catch (error) {
+    return;
+  }
+};
