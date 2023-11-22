9222ccaca10bb208a29d87892f5170364531665c
Tim Engle <timothy.englew@gmail.com>
2023-11-22 02:05:12 -0800

    ⛔️Created createExtendedError function

--- api/src/errors/helpers.ts
+++ api/src/errors/helpers.ts
@@ -1,3 +1,20 @@
 export class ExtendedError extends Error {
-  statusCode?: number;
+    statusCode?: number;
 }
+
+export const createExtendedError = ({
+    message,
+    statusCode,
+    stack
+}: {
+    message?: string;
+    statusCode?: number;
+    stack?: any;
+}) => {
+    const extendedError = new ExtendedError(
+        message ? message : `Internal Service Error`
+    );
+    extendedError.statusCode = statusCode;
+    extendedError.stack = stack;
+    return extendedError;
+};
