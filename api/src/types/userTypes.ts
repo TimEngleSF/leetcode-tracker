1a90c238b0b7ad240ffd6c093180d1d4fbb872af
Tim Engle <timothy.englew@gmail.com>
2023-11-21 19:26:58 -0800

    🙋Added groupes to UserDocument interface

--- api/src/types/userTypes.ts
+++ api/src/types/userTypes.ts
@@ -2,52 +2,53 @@ import { JwtPayload } from 'jsonwebtoken';
 import { Document, ObjectId } from 'mongodb';
 
 export interface UserDocument extends Document {
-  _id: ObjectId;
-  username: string;
-  displayUsername: string;
-  email: string;
-  firstName: string;
-  lastInit: string;
-  password: string;
-  status: 'pending' | 'verified';
-  verificationToken: string;
-  passwordToken: string | null;
-  questions: number[];
-  lastActivity: Date;
-}
-
-export interface UserLoginPayload {
-  user: {
-    _id: string;
+    _id: ObjectId;
     username: string;
+    displayUsername: string;
     email: string;
     firstName: string;
     lastInit: string;
-    lastActivity: Date;
+    password: string;
     status: 'pending' | 'verified';
-  };
-  token: string;
+    verificationToken: string;
+    passwordToken: string | null;
+    groups: ObjectId[];
+    questions: number[];
+    lastActivity: Date;
+}
+
+export interface UserLoginPayload {
+    user: {
+        _id: string;
+        username: string;
+        email: string;
+        firstName: string;
+        lastInit: string;
+        lastActivity: Date;
+        status: 'pending' | 'verified';
+    };
+    token: string;
 }
 
 export type CreateUserInService = {
-  displayUsername: string;
-  email: string;
-  password: string;
-  firstName: string;
-  lastInit: string;
+    displayUsername: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastInit: string;
 };
 
 export type CreateUserInDb = {
-  displayUsername: string;
-  email: string;
-  hashedPass: string;
-  firstName: string;
-  lastInit: string;
-  verificationToken: string;
+    displayUsername: string;
+    email: string;
+    hashedPass: string;
+    firstName: string;
+    lastInit: string;
+    verificationToken: string;
 };
 
 export interface UserToken extends JwtPayload {
-  userId: string;
-  email: string;
-  username: string;
+    userId: string;
+    email: string;
+    username: string;
 }
