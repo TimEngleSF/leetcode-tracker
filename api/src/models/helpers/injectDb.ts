ebafaa3476bb482abcc899c28a08dae81dfd4fce
Tim Engle <timothy.englew@gmail.com>
2023-11-21 19:53:37 -0800

    👯🗄️Added functionality to get groups collection

--- api/src/models/helpers/injectDb.ts
+++ api/src/models/helpers/injectDb.ts
@@ -1,8 +1,13 @@
 import { Collection, Db, Document } from 'mongodb';
 
 export const injectDb = <T extends Document>(
-  db: Db,
-  collectionName: 'users' | 'blacklistTokens' | 'questions' | 'questionData'
+    db: Db,
+    collectionName:
+        | 'users'
+        | 'blacklistTokens'
+        | 'questions'
+        | 'questionData'
+        | 'groups'
 ): Collection<T> => {
-  return db.collection<T>(collectionName);
+    return db.collection<T>(collectionName);
 };
