f1264ed6d30b27cf887415f1c63744e306117092
Tim Engle <timothy.englew@gmail.com>
2023-11-24 00:55:18 -0800

    🔐Updated registration prompt to have user confirm their email

--- cli/src/Auth/Prompts/registrationPrompt.ts
+++ cli/src/Auth/Prompts/registrationPrompt.ts
@@ -1,81 +1,103 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 import {
-  emailSchema,
-  usernameFirstNameSchema,
-  lastInitSchema,
-  passwordSchema,
+    emailSchema,
+    usernameFirstNameSchema,
+    lastInitSchema,
+    passwordSchema
 } from './validation/validationSchema.js';
 import { RegistrationPrompt } from '../../Types/prompts.js';
 
+const emailPrompt = async (): Promise<string> => {
+    const emailAnswers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter an email: ',
+            validate: (input) => {
+                const { error } = emailSchema.validate(input);
+                return error ? chalk.red(error.message) : true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'emailConfirm',
+            message: 'Confirm your email: ',
+            validate: (input) => {
+                const { error } = emailSchema.validate(input);
+                return error ? chalk.red(error.message) : true;
+            }
+        }
+    ]);
+
+    if (emailAnswers.email !== emailAnswers.emailConfirm) {
+        console.log(chalk.red('Emails did not match. Please try again.'));
+        return await emailPrompt();
+    }
+
+    return emailAnswers.email;
+};
+
 const passwordPrompt = async (): Promise<{ password: string } | null> => {
-  const passwordAnswers = await inquirer.prompt([
-    {
-      type: 'password',
-      name: 'password',
-      message: 'Please choose a simple password: ',
-      validate: (input) => {
-        const { error } = passwordSchema.validate(input);
-        return error ? chalk.red(error.message) : true;
-      },
-    },
-    {
-      type: 'password',
-      name: 'passwordCheck',
-      message: 'Please re-enter your password: ',
-    },
-  ]);
+    const passwordAnswers = await inquirer.prompt([
+        {
+            type: 'password',
+            name: 'password',
+            message: 'Please choose a simple password: ',
+            validate: (input) => {
+                const { error } = passwordSchema.validate(input);
+                return error ? chalk.red(error.message) : true;
+            }
+        },
+        {
+            type: 'password',
+            name: 'passwordCheck',
+            message: 'Please re-enter your password: '
+        }
+    ]);
 
-  if (passwordAnswers.password !== passwordAnswers.passwordCheck) {
-    console.log(chalk.red('Passwords did not match. Please try again.'));
-    return await passwordPrompt();
-  }
+    if (passwordAnswers.password !== passwordAnswers.passwordCheck) {
+        console.log(chalk.red('Passwords did not match. Please try again.'));
+        return await passwordPrompt();
+    }
 
-  return passwordAnswers.password;
+    return passwordAnswers.password;
 };
 
 const registrationPrompt = async (): Promise<RegistrationPrompt> => {
-  const answers = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Enter an email: ',
-      validate: (input) => {
-        const { error } = emailSchema.validate(input);
-        return error ? chalk.red(error.message) : true;
-      },
-    },
-    {
-      type: 'input',
-      name: 'username',
-      message: 'Enter a username: ',
-      validate: (input) => {
-        const { error } = usernameFirstNameSchema.validate(input);
-        return error ? chalk.red(error.message) : true;
-      },
-    },
-    {
-      type: 'input',
-      name: 'firstName',
-      message: "What's your first name? ",
-      validate: (input) => {
-        const { error } = usernameFirstNameSchema.validate(input);
-        return error ? chalk.red(error.message) : true;
-      },
-    },
-    {
-      type: 'input',
-      name: 'lastInit',
-      message: "What's your last initial? ",
-      validate: (input) => {
-        const { error } = lastInitSchema.validate(input);
-        return error ? chalk.red(error.message) : true;
-      },
-    },
-  ]);
+    const email = await emailPrompt();
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter a username: ',
+            validate: (input) => {
+                const { error } = usernameFirstNameSchema.validate(input);
+                return error ? chalk.red(error.message) : true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'firstName',
+            message: "What's your first name? ",
+            validate: (input) => {
+                const { error } = usernameFirstNameSchema.validate(input);
+                return error ? chalk.red(error.message) : true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastInit',
+            message: "What's your last initial? ",
+            validate: (input) => {
+                const { error } = lastInitSchema.validate(input);
+                return error ? chalk.red(error.message) : true;
+            }
+        }
+    ]);
 
-  const passwordAnswer = await passwordPrompt();
-  return { ...answers, password: passwordAnswer };
+    const passwordAnswer = await passwordPrompt();
+    return { email, ...answers, password: passwordAnswer };
 };
 
 export default registrationPrompt;
--- cli/src/Auth/registerUser.ts
+++ cli/src/Auth/registerUser.ts
@@ -6,43 +6,43 @@ import { API_URL } from '../config.js';
 import { RegistrationPrompt } from '../Types/prompts.js';
 
 const registerToAPI = async (answers: RegistrationPrompt) => {
-  const { username, email, firstName, lastInit, password } = answers;
-  const payload = {
-    username,
-    email,
-    firstName,
-    lastInit,
-    password,
-  };
-  try {
-    const { data } = await axios({
-      method: 'POST',
-      url: `${API_URL}/register`,
-      headers: { 'Content-Type': 'application/json' },
-      data: payload,
-    });
+    const { username, email, firstName, lastInit, password } = answers;
+    const payload = {
+        username,
+        email,
+        firstName,
+        lastInit,
+        password
+    };
+    try {
+        const { data } = await axios({
+            method: 'POST',
+            url: `${API_URL}/register`,
+            headers: { 'Content-Type': 'application/json' },
+            data: payload
+        });
 
-    return data;
-  } catch (error: any) {
-    return error.response?.data || 'An unexpected error occurred';
-  }
+        return data;
+    } catch (error: any) {
+        return error.response?.data || 'An unexpected error occurred';
+    }
 };
 
 const registerUser = async (): Promise<void> => {
-  const answers = await registrationPrompt();
-  const registerPayload = {
-    username: answers.username.trim(),
-    email: answers.email.trim(),
-    firstName: answers.firstName.trim(),
-    lastInit: answers.lastInit.trim(),
-    password: answers.password,
-  };
-  const data = await registerToAPI(registerPayload);
+    const answers = await registrationPrompt();
+    const registerPayload = {
+        username: answers.username.trim(),
+        email: answers.email.trim(),
+        firstName: answers.firstName.trim(),
+        lastInit: answers.lastInit.trim(),
+        password: answers.password
+    };
+    const data = await registerToAPI(registerPayload);
 
-  if (data.message) {
-    console.log(chalk.redBright(data.message, data.error));
-    return await registerUser();
-  }
+    if (data.message) {
+        console.log(chalk.redBright(data.message, data.error));
+        return await registerUser();
+    }
 };
 
 export default registerUser;
