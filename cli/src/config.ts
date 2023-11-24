7e72218f15cc1ebef32f8e8f7958c843ba75ad65
Tim Engle <timothy.englew@gmail.com>
2023-11-24 00:56:57 -0800

    🌐Updated API_URL to point to server

--- cli/src/config.ts
+++ cli/src/config.ts
@@ -1,15 +1,15 @@
 import { getPackageVersion } from './utils.js';
 
-export let API_URL = 'http://localhost:3000';
+export let API_URL = 'http://44.227.144.103';
 export let LATEST_VERSION = process.env.npm_package_version;
 
 export const setLatestVersion = async () => {
-  const actualLatestVersion = await getPackageVersion();
-  try {
-    if (actualLatestVersion !== LATEST_VERSION) {
-      LATEST_VERSION = actualLatestVersion;
+    const actualLatestVersion = await getPackageVersion();
+    try {
+        if (actualLatestVersion !== LATEST_VERSION) {
+            LATEST_VERSION = actualLatestVersion;
+        }
+    } catch (error) {
+        return;
     }
-  } catch (error) {
-    return;
-  }
 };
