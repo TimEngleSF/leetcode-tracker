5d06a7e0ad1fe7637aff448a32c710602b60bd8c
Tim Engle <timothy.englew@gmail.com>
2023-12-04 11:01:17 -0800

    ✍️Formatted to use 4 space indent instead of 2

--- cli/src/Leaderboard/general-leaderboard/helpers/utils.ts
+++ cli/src/Leaderboard/general-leaderboard/helpers/utils.ts
@@ -1,13 +1,13 @@
 import Table from 'cli-table3';
 import chalk from 'chalk';
 export const initGeneralTable = () => {
-  return new Table({
-    head: [
-      chalk.white('Rank'),
-      chalk.white('User'),
-      chalk.white('Passed'),
-      chalk.white('Last Active'),
-    ],
-    colWidths: [8, 10, 8, 15],
-  });
+    return new Table({
+        head: [
+            chalk.white('Rank'),
+            chalk.white('User'),
+            chalk.white('Passed'),
+            chalk.white('Last Active')
+        ],
+        colWidths: [8, 10, 8, 15]
+    });
 };
