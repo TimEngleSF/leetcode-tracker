bb7e3f7c873b521c5d13467e35c82b751ad16fac
Tim Engle <timothy.englew@gmail.com>
2023-12-04 11:00:42 -0800

    🏆Added langauge column to question leaderboard

--- cli/src/Leaderboard/question-leaderboard/helpers/utils.ts
+++ cli/src/Leaderboard/question-leaderboard/helpers/utils.ts
@@ -2,39 +2,40 @@ import chalk from 'chalk';
 import Table from 'cli-table3';
 
 export const initQuestTable = () => {
-  return new Table({
-    head: [
-      chalk.white('Rank'),
-      chalk.white('User'),
-      chalk.white('Times Passed'),
-      chalk.white('Speed'),
-      chalk.white('Most Recent Pass'),
-    ],
-    colWidths: [8, 10, 15, 8],
-  });
+    return new Table({
+        head: [
+            chalk.white('Rank'),
+            chalk.white('User'),
+            chalk.white('Passed'),
+            chalk.white('Speed'),
+            chalk.white('Language'),
+            chalk.white('Most Recent')
+        ],
+        colWidths: [8, 15, 10, 10, 15, 16]
+    });
 };
 
 interface DataRow {
-  _id: string;
-  firstName: string;
-  lastInit: string;
-  passedCount: number;
-  minSpeed?: number;
-  mostRecent: Date;
+    _id: string;
+    firstName: string;
+    lastInit: string;
+    passedCount: number;
+    minSpeed?: number;
+    mostRecent: Date;
 }
 export const sortLeaderboardData = (
-  data: DataRow[],
-  sortingSelection: string
+    data: DataRow[],
+    sortingSelection: string
 ) => {
-  if (sortingSelection === 'minSpeed') {
-    return data.sort((a, b) => {
-      if (a.minSpeed && b.minSpeed) {
-        return a.minSpeed - b.minSpeed;
-      } else {
-        return 0;
-      }
-    });
-  } else {
-    return data.sort((a, b) => b.passedCount - a.passedCount);
-  }
+    if (sortingSelection === 'minSpeed') {
+        return data.sort((a, b) => {
+            if (a.minSpeed && b.minSpeed) {
+                return a.minSpeed - b.minSpeed;
+            } else {
+                return 0;
+            }
+        });
+    } else {
+        return data.sort((a, b) => b.passedCount - a.passedCount);
+    }
 };
